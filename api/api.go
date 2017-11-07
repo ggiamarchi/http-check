4f38abfaac6395f960e750f66af5892b67811459
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2017-11-07 16:15:42 +1100

    Basic authorization check based on a secret jey

--- api/api.go
+++ api/api.go
@@ -1,11 +1,23 @@
 package api
 
 import (
+	"os"
+
 	"gopkg.in/gin-gonic/gin.v1"
 )
 
+func checkAuthorization(c *gin.Context) {
+	secretKeyValues := c.Request.Header["X-Secret-Key"]
+	if len(secretKeyValues) != 1 || secretKeyValues[0] != os.Getenv("HTTP_CHECK_SECRET_KEY") {
+		c.JSON(403, gin.H{"message": "unauthorized"})
+		c.Abort()
+	}
+}
+
 func CreateEngine() *gin.Engine {
 	engine := gin.Default()
+
+	engine.Use(checkAuthorization)
 
 	engine.GET("/df", df)
 	engine.HEAD("/df", df)
